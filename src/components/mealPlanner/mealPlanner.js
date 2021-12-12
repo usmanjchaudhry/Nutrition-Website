898422bed1deed5ce48a3e635190cd6d0eb5abc8
usmanjchaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2021-12-12 15:39:14 -0800

    Cleaned up GUI

--- src/components/mealPlanner/mealPlanner.js
+++ src/components/mealPlanner/mealPlanner.js
@@ -33,13 +33,18 @@ function Mealplanner() {
     return (
 <div className="App">
   <section className="controls">
+  <h1 class="home__title">Want Better Meals?</h1>
+  <img src="img/MealPrepFixed.png" alt="" class="mealPlan__img"/>
+    <h1>Plan out your day with a daily meal plan</h1>
+  <h4>For more information about daily caloric intake click <u className="oneWord"> <a href = 'https://www.fda.gov/media/112972/download'>here</a></u></h4>
     <input
+      class="mealplannerInput"
       type="number"
       placeholder="Calories (e.g. 2000)"
       onChange={handleChange}
     />
     <button onClick={getMealData} class="input">Get Daily Meal Plan</button>
-
+   
   </section>
   {mealData && <MealList mealData={mealData} />}
 </div>
